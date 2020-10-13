78544ed384fb5820ea080cae6bcb183b8a687e7b
dekim1028 <daeun235194@gmail.com>
2020-10-13 15:51:30 +0900

    Implement API linkage with axios and redux-saga

--- blog-frontend/src/lib/api/auth.js
+++ blog-frontend/src/lib/api/auth.js
@@ -1,4 +1,13 @@
-import client from './client';
+import client from "./client"
 
-export const login = ({userid,password})=>
-    client.post('/api/auth/login',{userid,password});
+export const login = ({userid,password})=>{
+    client.post("/api/auth/login",{userid,password});
+}
+
+export const signup = ({userid,password})=>{
+    client.post("/api/auth/signup",{userid,password});
+}
+
+export const check = () =>{
+    client.get("/api/auth/check");
+}
